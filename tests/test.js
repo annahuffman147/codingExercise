cd7ab70cc43d5343988194abe6ce5762df15b4ab
annahuffman147 <annahuffman.plc@gmail.com>
2016-05-26 21:27:07 -0600

    valuable attempt, learned that you cannot .split a number

--- tests/test.js
+++ tests/test.js
@@ -4,9 +4,9 @@ var code = require('../main.js')
 
 describe('changeFromString', function(){
   it('should change string to number', function(){
-    expect(code.changeFromString('45')).to.equal(45)
-    expect(code.changeFromString(35)).to.equal('not a string')
-    expect(code.changeFromString(45.22)).to.equal('not a string')
+    expect(code.changeFromString('45'), 45)
+    expect(code.changeFromString(35), 'not a string')
+    expect(code.changeFromString(45.22), 'not a string')
   })
 });
 
@@ -14,5 +14,12 @@ describe('singleDigitToWord', function(){
   it('should convert a single digit to a word', function(){
     expect(code.singleDigitToWord(1), 'one')
     expect(code.singleDigitToWord(9), 'nine')
+  })
+});
+
+describe('doubleDigitToWord', function(){
+  it('should convert a double digit that is above 19 to a word', function(){
+    expect(code.doubleDigitToWord('45'), 'twenty-one')
+    expect(code.doubleDigitToWord(39), 'thirty-nine')
   })
 })
